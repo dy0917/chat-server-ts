586cb74a8b9037d427f1782bda4e9a6e86d0947b
dy0917 <huangkingsley@gmail.com>
2025-11-04 22:55:52 +1300

    update val

--- src/env.ts
+++ src/env.ts
@@ -2,3 +2,4 @@ export const nodeEnv = process.env.NODE_ENV;
 export const encryptionKey = process.env.ENCRYPTION_KEY;
 export const dbUrl = process.env.DB_URI;
 export const port = process.env.PORT;
+console.log("dbUrl", dbUrl);
